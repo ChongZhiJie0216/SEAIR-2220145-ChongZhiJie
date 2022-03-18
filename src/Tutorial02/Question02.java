631efab30cd61dfbf27aabda54ebf710c66658ff
ChongZhiJie <ChongZhiJie021604@gmail.com>
2022-03-18 13:13:05 +0800

    Updated Tutorial03 Question 02 and Rebuild Tutorial 02 Question 02

--- src/Tutorial02/Question02.java
+++ src/Tutorial02/Question02.java
@@ -9,17 +9,29 @@ public class Question02 {
         String name = scanner.nextLine();
         System.out.println("Where are you from?");
         String country=scanner.next();
+
+        switch (country) {
+            case "Malaysia" -> System.out.println("Selamat Datang," + name);
+            case "Japan" -> System.out.println("Irashaimase ," + name);
+            case "China" -> System.out.println("Ni Hao," + name);
+            case "France" -> System.out.println("Boujour," + name);
+            default -> System.out.println("Welcome," + name);
+        }
+
+        /*
+
         if (country.equals("Malaysia")){
-            System.out.println("Selamat Datang ,"+name);
+           System.out.println("Selamat Datang ,"+name);
         }else if (country.equals("Japan")) {
             System.out.println("Irashaimase ,"+ name);
         }else if (country.equals("China")){
-            System.out.println("Ni Hao ,"+name);
+           System.out.println("Ni Hao ,"+name);
         }else if (country.equals("France")){
             System.out.println("Boujour ,"+name);
         }else{
             System.out.println("Welcome ," +name);
         }
+         */
         scanner.close();
     }
 }
--- src/Tutorial03/Question02.java
+++ src/Tutorial03/Question02.java
@@ -1,7 +1,23 @@
 package Tutorial03;
 
+import java.util.Scanner;
+
 public class Question02 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter your gender (F/M): ");
+            if (scanner.next().equals("F")) {
+                total++;
+            }
+        }
+        System.out.println("Total of Male: " + (10 - total));
+        System.out.println("Total of Female: " + total);
     }
 }
+
+/*
+for (开始(从哪里开始); 条件(达到什么条件时候停止); 每运行一次的做一次结果) {}
+while (条件(一直运行到符合条件为止)) {}
+ */
