fcdd2e1820f6c511a8b40c142d8e1f50d053b5c9
ChongZhiJie <ChongZhiJie021604@gmail.com>
2022-04-01 10:22:17 +0800

    Update Tutorial 04 Question01 Create Tutorial 05 Folder and Question

--- src/Tutorial04/Question01.java
+++ src/Tutorial04/Question01.java
@@ -6,53 +6,33 @@ import java.util.Scanner;
 public class Question01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String operator;
-        String lastOperator = "";
-        String resultString = "";
-        int answer = 0;
-        do{
-            System.out.println("Enter number:");
-            int input = scanner.nextInt();
 
-            System.out.println("Choose an operator (+ or – or * or =) :");
+        System.out.print("Ender a Number:");
+        int number = scanner.nextInt();
+        String operator = "";
+        int answer = number;
+        String formula = String.valueOf(number);
+
+        while (!operator.equals("=")) {
+            System.out.print("Enter Operator(+ or - or * or =)");
             operator = scanner.next();
-
-            switch (operator.equals("=") ? lastOperator : operator) {
-                case "+":
-                    answer += input;
-                    resultString += input + " + ";
-                    break;
-                case "-":
-                    answer -= input;
-                    resultString += input + " - ";
-                    break;
-                case "*":
-                    answer *= input;
-                    resultString += input + " * ";
-                    break;
+            if (operator.equals("=")) {
+                break;
             }
-            if(operator.equals("=")) {
-                resultString = resultString.substring(0, resultString.length() - 3);
-                System.out.println(resultString + " = " + answer);
+            System.out.print("Enter a Number:");
+            number = scanner.nextInt();
+            if (operator.equals("+")) {
+                answer += number;
+                formula += " " + operator + " " + number;
+            } else if (operator.equals("-")) {
+                answer -= number;
+                formula += " " + operator + " " + number;
+            } else if (operator.equals("*")) {
+                answer *= number;
+                formula += " " + operator + " " + number;
             }
-            lastOperator = operator;
-        }while (!operator.equals("="));
+        }
+        System.out.println("Answer is :" + formula + " = " + answer);
         scanner.close();
-
-//       d o{
-//            System.out.println("Enter number:");
-//            int num01 = scanner.nextInt();
-//            System.out.println("Choose an operator (+ or – or * or =) :");
-//            operator = scanner.next();
-//            System.out.println("Enter number:");
-//            int num02 = scanner.nextInt();
-//            int answer = 0;
-//            switch (operator){
-//                case "+":answer = num01+num02;break;
-//                case "-":answer = num01-num02;break;
-//                case "*":answer = num01*num02;break;
-//                default:System.out.println("输入不合法");
-//            }
-//    }while(!operator.equals("="));
     }
 }
---
+++ src/Tutorial05/Question02.java
@@ -0,0 +1,7 @@
+package Tutorial05;
+
+public class Question02 {
+    public static void main(String[] args) {
+
+    }
+}
